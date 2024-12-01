611cebb37911f8fcb1aac6a653d1d2ba33b8029f
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-12-01 15:50:01 -0800

    EntradaDeDados
    Aula de entrada de dados explicando como usar e com exemplos

---
+++ EntradaDeDados.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class EntradaDeDados
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite oq vc desje: ");
+            string frase = Console.ReadLine();
+    
+            //Entrada de Dados
+            string x = Console.ReadLine();
+            string y = Console.ReadLine(); 
+            string z = Console.ReadLine();
+
+            //Usando VETORES
+            string s = Console.ReadLine();
+            string[] v = s.Split(' '); // string[] v = Console.ReadLine().Split(' '); posso fz dessa forma sem o string s no começo
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+
+
+            //Saida de Dados
+            Console.WriteLine("Voce Digitou:  ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+
+        }
+    }
+}
