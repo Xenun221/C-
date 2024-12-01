364209f6f12be4b9fa9b8d4780abc66accc332f4
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-12-01 15:48:27 -0800

    EntradaDeDados2
    Aula de EntradaDeDados com exemplo

---
+++ EntradaDeDados2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class EntradaDeDados2
+    {
+        static void Main(string[] args)
+        {
+            //Sempre converter com Parse pois o Console.ReadLine vai enteder que e uma String
+            int num1 = int.Parse(Console.ReadLine());
+            char sexo = char.Parse(Console.ReadLine());
+            double n2= double.Parse(Console.ReadLine());
+
+            //USANDO O SPLIT
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            int idade = int.Parse(vet[1]);
+            double altura = double.Parse(vet[2]);
+            double n0 = double.Parse(vet[4]);
+
+            //Ler os dados
+            Console.WriteLine("Vode Digitou: ");
+            Console.WriteLine(num1);
+            Console.WriteLine(sexo);
+            Console.WriteLine(n2);
+
+            //Na mesma linha usando o split
+            Console.WriteLine(altura);  
+            Console.WriteLine(nome);  
+            Console.WriteLine(idade);
+            Console.WriteLine(n0);
+           
+        }
+    }
+}
