2bbb1109bf91601f5cd30011006be886e596616d
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-12-01 15:53:17 -0800

    SaidaDeDados
    Aula de saida de dados como usar com exeplos

---
+++ SaidaDeDados.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class SaidaDeDados
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*Com quebra de linha */
+            Console.WriteLine("Ola Mundo!");
+
+            /*Sem quebra de linha:  */
+            Console.Write("Olá Amigo! ");
+            Console.Write("Oi Mundo!");
+
+            /*Para ler numeros com ponto flutuantes*/
+            double x = 10.35784;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.ToString("F4"));
+
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            /*Para concatenar vários elementos*/
+            Console.WriteLine("Resultado = " + x);
+            Console.WriteLine("O valor do troco é " + x + " reais");
+            Console.WriteLine("O valor de troco é " + x.ToString("F2") + " reais");
+
+            Console.ReadLine();
+
+        }
+    }
+}
